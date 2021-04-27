70378c150e17f6b7086250f6275fc858f1f10a1a
Theodora2000 <xsimont@stuba.sk>
2021-04-27 09:52:01 +0200

    .gitignore spring initializer

---
+++ src/main/java/sk/stuba/uim/fei/oop/cvicenie10/Cvicenie10Application.java
@@ -0,0 +1,13 @@
+package sk.stuba.uim.fei.oop.cvicenie10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvicenie10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvicenie10Application.class, args);
+    }
+
+}
---
+++ src/test/java/sk/stuba/uim/fei/oop/cvicenie10/Cvicenie10ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.uim.fei.oop.cvicenie10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvicenie10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
